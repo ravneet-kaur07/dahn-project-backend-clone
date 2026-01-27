9df66f765a51c267ec5606dfec9e40b09b391cf7
Ravneet Kaur <ravneet1107@gmail.com>
2026-01-27 15:25:48 +0530

    Add required dependencies

--- model/users.js
+++ model/users.js
@@ -35,7 +35,7 @@ UserSchema.pre("save", async function (next){
     }
     const salt= await bcrypt.genSalt(10);
     this.password= await bcrypt.hash(this.password, salt);
-    next();
+    // next();
 })
 
 const User= mongoose.model("User", UserSchema);
