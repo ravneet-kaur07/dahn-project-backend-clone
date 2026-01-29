5651aca2c8ca490f02ae13572b284d0018f4a22d
Ravneet <nessfiraveenakaur2026@gmail.com>
2026-01-29 17:54:46 +0530

    Add cors

--- server.js
+++ server.js
@@ -4,10 +4,12 @@ import authRouter from './routes/authRoutes.js';
 import dashRouter from './routes/dashRoutes.js';
 import connectDB from './config/db.js'; 
 import profileRouter from './routes/profileRoutes.js';
+import cors from 'cors';
 
 dotenv.config();
 const app= express();
 app.use(express.json());
+app.use(cors());
 app.use('/', authRouter);
 app.use('/dashboard', dashRouter);
 app.use('/profile', profileRouter);
