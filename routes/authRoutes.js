db6c9903b06453e3ad23a9efba78a3d188b6d230
Ravneet <nessfiraveenakaur2026@gmail.com>
2026-02-02 13:31:38 +0530

    Add session restore endpoint for page refresh

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -1,11 +1,18 @@
 import express from 'express';
 import { register, login } from '../controller/authController.js';
-// import { userAuth } from '../middlewares/authMiddleware.js';
+import { userAuth } from '../middlewares/authMiddleware.js';
 // import { rbacMiddleware } from '../middlewares/rbacMiddleware.js';
 
 const authRouter= express.Router();
 authRouter.post('/register', register);
 authRouter.post('/login', login);
+authRouter.get("/validate", userAuth, (req, res)=>{
+    res.json({
+        id: req.user.id,
+        email: req.user.email,
+        role: req.user.role,
+    })
+})
 // authRouter.get('/dash',userAuth, getDash);
 // authRouter.get('/staff',userAuth, rbacMiddleware("agency"), getStaff);
 
